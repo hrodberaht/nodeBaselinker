e018c8274b441c4fd9d94bfa8e8ece5586f7cc9f
hrodberaht <robpszczolka@gmail.com>
2018-08-20 21:55:36 +0200

    generat array with product from orders

--- app.js
+++ app.js
@@ -6,16 +6,34 @@ const config = require("./config");
 const app = new Koa();
 
 const { token } = config;
+const products = [];
 
 app.use(async ctx => {
     const res = await fetch('https://api.baselinker.com/connector.php',
     {
         method: 'POST',
         headers: {'Content-Type':'application/x-www-form-urlencoded'},
-        body: `token=${token}&method=getOrders&parameters={"status_id": 30594}`
+        body: `token=${token}&method=getOrders&parameters={"status_id": 50041}`
     });
     const json = await res.json();
-    ctx.body = json.orders;
+
+    json.orders.map((prods) => {
+        prods.products.map( (prods)=> {
+            products.push(prods)
+        })
+    })
+
+    let eans = [];
+    products.map((prod)=>{
+        eans.push({
+            name: prod.name,
+            ean: prod.ean,
+            quantity: prod.quantity,
+            totalPrice: prod.price_brutto*prod.quantity
+
+        })
+    })
+    ctx.body = eans;
 })
 
 app.listen(3000,() => {
