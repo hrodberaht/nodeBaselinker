9d1f46e4702d1aae655b3ce4a197ddd8772fc423
hrodberaht <robpszczolka@gmail.com>
2018-08-20 20:33:16 +0200

    connect to baselinker

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const Koa = require("koa");
 const fetch = require("node-fetch");
 
+const config = require("./config");
 
 const app = new Koa();
 
+const { token } = config;
+
 app.use(async ctx => {
-    const res = await fetch('https://api.github.com/users/github');
+    const res = await fetch('https://api.baselinker.com/connector.php',
+    {
+        method: 'POST',
+        headers: {'Content-Type':'application/x-www-form-urlencoded'},
+        body: `token=${token}&method=getOrders&parameters={"status_id": 30594}`
+    });
     const json = await res.json();
-    ctx.body = json;
+    ctx.body = json.orders;
 })
 
 app.listen(3000,() => {
